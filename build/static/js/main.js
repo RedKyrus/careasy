46dbad30be2046f343e3a2d7a2547ee3451d0fa9
RedKyrus <ahehfldi007@gmail.com>
2022-01-18 09:16:07 +0900

    modal update

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -67,20 +67,6 @@ let onready = () =>{
     }
   });
 
-  modalOpenEvent("agree");
-  modalOpenEvent("quote");
-  modalOpenEvent("complete");
-  modalOpenEvent("branch");
-
-  modalCloseEvent();
-
-  topAdCloseEvent();
-
-
-  scrollMoveEvent();
-
-  youtubeControll();
-
   window.addEventListener('resize', rearrangementSpecial)
 
 }
@@ -112,7 +98,7 @@ let rearrangementSpecial = function(){
     else self.name = '';
 
     isRsizeMobileReady = true;
-    console.log("작동PC");
+    //console.log("작동PC");
     //요소생성
     let tampTag = document.createElement('div');
     tampTag.innerHTML = "<div class='swiper-slide slide-part'></div>";
@@ -169,73 +155,6 @@ let rearrangementSpecial = function(){
   
     isRsizeMobileReady = false;
   }
-}
-
-
-let modalOpenEvent = (modalname) =>{
-  let btnOpenList = document.querySelectorAll(`.fn-open-modal-${modalname}`);
-  let tagetModal = document.querySelector(`.modal-${modalname}`);
-  
-  let openModal= () =>{
-    tagetModal.classList.add("modal-show");
-    console.log(modalname, "작동");
-  }
-
-  btnOpenList.forEach(btn =>{
-    btn.addEventListener("click", openModal);
-  });
-
-}
-
-let modalCloseEvent =() =>{
-  let btnCloseList = document.querySelectorAll(".fn-close-modal");
-  //btnClose.closest(".modal-wrap");
-  
-  btnCloseList.forEach(btn =>{
-    let tagetModal = btn.closest(".modal-wrap");
-    btn.addEventListener("click", ()=>{
-      tagetModal.classList.remove("modal-show");  
-    });
-  });
-  
-}
-
-let topAdCloseEvent =() =>{
-  let topAd = document.querySelector(".top-ad");
-  let btnCloseAd = document.querySelector(".top-ad .btn-close-ad");
-  //btnClose.closest(".modal-wrap");
-  
-  let closeTopAd = () =>{
-    topAd.classList.add("ad-hidden");
-  }
-
-  btnCloseAd.addEventListener("click",closeTopAd)
-  
-}
-
-let scrollMoveEvent = () =>{
-  let btnTrigger = document.querySelectorAll("[data-scroll-target]");
-  //let tagetList = document.querySelectorAll("[data-scroll-id]");
-  
-  
-
-  btnTrigger.forEach(btn=>{
-    let targetID = btn.getAttribute("data-scroll-target");
-    let target = document.querySelector(`[data-scroll-id='${targetID}']`);
-    
-    if(target === null) return;
-
-    //console.dir(target);
-    
-
-    btn.addEventListener("click",()=>{
-      //window.scrollTo({top:pos, behavior:'smooth'});
-      let pos = target.offsetTop - 20;
-      window.scrollTo({top:pos, behavior:'smooth'});
-    });
-  });
-
-
 }
 
 
---
+++ src/assets/js/common.js
@@ -0,0 +1,87 @@
+window.addEventListener('DOMContentLoaded', () =>{
+
+  modalOpenEvent("agree");
+  modalOpenEvent("quote");
+  modalOpenEvent("complete");
+  modalOpenEvent("branch");
+
+  modalCloseEvent();
+
+  topAdCloseEvent();
+
+
+  scrollMoveEvent();
+
+  youtubeControll();
+
+}
+);
+
+
+
+
+let modalOpenEvent = (modalname) =>{
+  let btnOpenList = document.querySelectorAll(`.fn-open-modal-${modalname}`);
+  let tagetModal = document.querySelector(`.modal-${modalname}`);
+  
+  let openModal= () =>{
+    tagetModal.classList.add("modal-show");
+    console.log(modalname, "작동");
+  }
+
+  btnOpenList.forEach(btn =>{
+    btn.addEventListener("click", openModal);
+  });
+
+}
+
+let modalCloseEvent =() =>{
+  let btnCloseList = document.querySelectorAll(".fn-close-modal");
+  //btnClose.closest(".modal-wrap");
+  
+  btnCloseList.forEach(btn =>{
+    let tagetModal = btn.closest(".modal-wrap");
+    btn.addEventListener("click", ()=>{
+      tagetModal.classList.remove("modal-show");  
+    });
+  });
+  
+}
+
+let topAdCloseEvent =() =>{
+  let topAd = document.querySelector(".top-ad");
+  let btnCloseAd = document.querySelector(".top-ad .btn-close-ad");
+  //btnClose.closest(".modal-wrap");
+  
+  let closeTopAd = () =>{
+    topAd.classList.add("ad-hidden");
+  }
+
+  btnCloseAd.addEventListener("click",closeTopAd)
+  
+}
+
+let scrollMoveEvent = () =>{
+  let btnTrigger = document.querySelectorAll("[data-scroll-target]");
+  //let tagetList = document.querySelectorAll("[data-scroll-id]");
+  
+  
+
+  btnTrigger.forEach(btn=>{
+    let targetID = btn.getAttribute("data-scroll-target");
+    let target = document.querySelector(`[data-scroll-id='${targetID}']`);
+    
+    if(target === null) return;
+
+    //console.dir(target);
+    
+
+    btn.addEventListener("click",()=>{
+      //window.scrollTo({top:pos, behavior:'smooth'});
+      let pos = target.offsetTop - 20;
+      window.scrollTo({top:pos, behavior:'smooth'});
+    });
+  });
+
+
+}
